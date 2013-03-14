7266cfd922f076d0c8783c77e1363c2d8285026d
Lorenz Moesenlechner <moesenle@in.tum.de>
2013-03-14 20:27:24 +0100

    Added a little more pretty printing.

--- src/test_generic_message.cc
+++ src/test_generic_message.cc
@@ -9,19 +9,19 @@ using namespace generic_message;
 
 struct type_visitor : public boost::static_visitor<> {
   void operator()(const BaseType &type) const {
-    std::cout << "base type: " << type.type << std::endl;
+    std::cout << "<base type " << type.type << ">";
   }
   void operator()(const MessageType &type) const {
-    std::cout << "message type: " << type.package << "-" << type.name << std::endl;
+    std::cout << type.package << "/" << type.name;
   }
   void operator()(const BaseTypeArray &type) const {
-    std::cout << "base type array " << type.type.type << " "
-              << (type.size ? *type.size : -1 ) << std::endl;
+    std::cout << "<base type " << type.type.type << ">["
+              << type.size.get_value_or(0) << "]";
   }
   void operator()(const MessageTypeArray &type) const {
-    std::cout << "message type array " << type.type.package << " "
-              << type.type.name << " "
-              << (type.size ? *type.size : -1 ) << std::endl;
+    std::cout << type.type.package << "/"
+              << type.type.name << "["
+              << type.size.get_value_or(999) << "]";
   }
 };
 
@@ -47,8 +47,16 @@ int main(int argc, char *argv[]) {
   if (parse_message(message, &parsed_message)) {
     std::cout << "Parsing ok" << std::endl;
   }
+  std::cout << "Values:" << std::endl;
   for (size_t i = 0; i < parsed_message.fields.size(); i++) {
-    std::cout << parsed_message.fields[i].name << ": ";
     boost::apply_visitor(type_visitor(), parsed_message.fields[i].type);
+    std::cout << " " << parsed_message.fields[i].name << std::endl;
   }
+  std::cout << std::endl << "Constants:" << std::endl;
+  for (size_t i = 0; i < parsed_message.constants.size(); i++) {
+    std::cout << parsed_message.constants[i].name << " ";
+    boost::apply_visitor(type_visitor(), parsed_message.constants[i].type);
+    std::cout << " = '" << parsed_message.constants[i].value << "'" << std::endl;
+  }
+  return 0;
 }
